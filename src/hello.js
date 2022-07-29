43454f296324705dc2e5e292e07d732d62d02573
jamal-brown <90861761+jamal-brown@users.noreply.github.com>
2022-07-29 19:40:19 -0400

    Update hello.js
    breaking on purpose

--- src/hello.js
+++ src/hello.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $.ajax({
-        url: "https://jar-spring-rest-service.azurewebsites.net/greeting"
+        url: "https://xjar-spring-rest-service.azurewebsites.net/greeting"
     }).then(function(data, status, jqxhr) {
        $('.greeting-id').append(data.id);
        $('.greeting-content').append(data.content);
