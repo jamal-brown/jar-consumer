1a4623db1e23b2f39c707716eb4d92293e821a8b
jamal-brown <90861761+jamal-brown@users.noreply.github.com>
2022-07-30 00:29:56 -0400

    Update hello.js

--- src/hello.js
+++ src/hello.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $.ajax({
-        url: "http://localhost:8080/greeting"
+        url: "https://jar-spring-rest-service.azurewebsites.net/greeting"
     }).then(function(data, status, jqxhr) {
        $('.greeting-id').append(data.id);
        $('.greeting-content').append(data.content);
