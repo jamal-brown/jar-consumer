638328be33691aa782d4ec3a941a956cfaebf5af
jamal-brown <90861761+jamal-brown@users.noreply.github.com>
2022-08-01 09:43:26 -0400

    Update hello.js

--- src/hello.js
+++ src/hello.js
@@ -1,9 +1,22 @@
+/*
 $(document).ready(function() {
     $.ajax({
         url: "https://jar-spring-rest-service.azurewebsites.net/greeting"
     }).then(function(data, status, jqxhr) {
        $('.greeting-id').append(data.id);
        $('.greeting-content').append(data.content);
+       console.log(jqxhr);
+    });
+*/
+
+$(document).ready(function() {
+    $.ajax({
+        url: "https://jar-spring-rest-service.azurewebsites.net/greeting"
+        //url:"http://localhost:5000/api/todo"
+    }).then(function(data, status, jqxhr) {
+       $('.item-id').append(data.id);
+       $('.item-name').append(data.name);
+       $('.item-isComplete').append(data.isComplete);
        console.log(jqxhr);
     });
 });
